2b4a5dd1a2bcbd00aacc40b706fe9ab9389395ea
fardog <nate@milkandtang.com>
2023-02-27 21:20:53 -0800

    additional primebot tests

--- primebot_test.go
+++ primebot_test.go
@@ -2,6 +2,7 @@ package primebot
 
 import (
 	"context"
+	"errors"
 	"math/big"
 	"testing"
 	"time"
@@ -114,6 +115,19 @@ func TestPrimebot(t *testing.T) {
 		t.Errorf("expected 7 to be posted, got %s", posted)
 	}
 
+	select {
+	case <-pc:
+		t.Fatal("got new post, when none should've been posted yet")
+	default:
+	}
+
+	tc <- time.Now()
+	gc <- big.NewInt(11)
+	posted = <-pc
+	if posted.Cmp(big.NewInt(11)) != 0 {
+		t.Errorf("expected 11 to be posted, got %s", posted)
+	}
+
 	cancel()
 
 	select {
@@ -121,5 +135,149 @@ func TestPrimebot(t *testing.T) {
 		break
 	case e := <-err:
 		t.Fatal(e)
+	}
+}
+
+func TestPrimebotFetchFailure(t *testing.T) {
+	fe := make(chan error, 1)
+	fetcher := &MockFetcher{
+		make(chan *Status),
+		fe,
+	}
+	ticker := &MockTicker{
+		make(chan time.Time),
+		make(chan time.Duration),
+		time.Second * 10,
+	}
+	generator := &MockGenerator{
+		make(chan *big.Int),
+		make(chan error),
+	}
+	pc := make(chan *big.Int, 1)
+	poster := &MockPoster{
+		pc,
+		make(chan error),
+	}
+	bot := NewPrimeBot(fetcher, ticker, generator, poster, nil)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err := make(chan error)
+	go func() {
+		if e := bot.Start(ctx); e != nil {
+			err <- e
+		}
+	}()
+
+	fe <- errors.New("frig")
+
+	e := <-err
+	if e.Error() != "frig" {
+		t.Errorf("got unexpected error message %s", e)
+	}
+
+	select {
+	case <-pc:
+		t.Fatal("got post, when none should've been posted")
+	default:
+	}
+
+}
+
+func TestPrimebotGeneratorFail(t *testing.T) {
+	fc := make(chan *Status, 1)
+	fetcher := &MockFetcher{
+		fc,
+		make(chan error, 1),
+	}
+	ticker := &MockTicker{
+		make(chan time.Time, 1),
+		make(chan time.Duration, 1),
+		time.Second * 10,
+	}
+	ge := make(chan error, 1)
+	generator := &MockGenerator{
+		make(chan *big.Int),
+		ge,
+	}
+	pc := make(chan *big.Int, 1)
+	poster := &MockPoster{
+		pc,
+		make(chan error, 1),
+	}
+	bot := NewPrimeBot(fetcher, ticker, generator, poster, nil)
+
+	fc <- &Status{
+		LastStatus: big.NewInt(5),
+		Posted:     time.Now(),
+	}
+	ge <- errors.New("frig")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err := make(chan error)
+	go func() {
+		if e := bot.Start(ctx); e != nil {
+			err <- e
+		}
+	}()
+
+	e := <-err
+	if e.Error() != "frig" {
+		t.Errorf("got unexpected error message %s", e)
+	}
+
+	select {
+	case <-pc:
+		t.Fatal("got post, when none should've been posted")
+	default:
+	}
+}
+
+func TestPrimebotPostFail(t *testing.T) {
+	fc := make(chan *Status, 1)
+	fetcher := &MockFetcher{
+		fc,
+		make(chan error),
+	}
+	tc := make(chan time.Time, 1)
+	dc := make(chan time.Duration, 1)
+	ticker := &MockTicker{
+		tc,
+		dc,
+		time.Second * 10,
+	}
+	gc := make(chan *big.Int, 1)
+	generator := &MockGenerator{
+		gc,
+		make(chan error),
+	}
+	pe := make(chan error, 1)
+	poster := &MockPoster{
+		make(chan *big.Int, 1),
+		pe,
+	}
+	bot := NewPrimeBot(fetcher, ticker, generator, poster, nil)
+
+	fc <- &Status{
+		LastStatus: big.NewInt(5),
+		Posted:     time.Now(),
+	}
+	tc <- time.Now()
+	gc <- big.NewInt(7)
+	pe <- errors.New("frig")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err := make(chan error)
+	go func() {
+		if e := bot.Start(ctx); e != nil {
+			err <- e
+		}
+	}()
+
+	e := <-err
+	if e.Error() != "frig" {
+		t.Errorf("got unexpected error message %s", e)
 	}
 }
